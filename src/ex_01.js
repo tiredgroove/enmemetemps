1f676b43163300593cfc5381173b2079eb49f2b1
tiredgroove <tiredgroove@gmail.com>
2023-04-19 02:33:23 +0700

    shader applied

--- src/ex_01.js
+++ src/ex_01.js
@@ -4,161 +4,117 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 
 if (WEBGL.isWebGLAvailable()) {
 //////
-const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0xffffff );
-scene.fog = new THREE.Fog( 0xffffff, 2000, 3500 );
 
-const camera = new THREE.PerspectiveCamera( 27, window.innerWidth / window.innerHeight, 1, 3500 );
-camera.position.z = 250;
+    ///scene
+  const scene = new THREE.Scene();
+	// scene.background = new THREE.Color( 0xffffff );
+  // scene.fog = new THREE.Fog( 0xffffff, 2000, 3500 );
+    //camera
+  const camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 10 );
+	camera.position.z = 2;
+    //renderer
+	const renderer = new THREE.WebGLRenderer();
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.outputEncoding = THREE.sRGBEncoding;
-document.body.appendChild( renderer.domElement );
+		// geometry
+  const geometry = new THREE.InstancedBufferGeometry();
+	
+  const vector = new THREE.Vector4();
 
-const ambientLight = new THREE.AmbientLight( 0x000000, 1);
-scene.add( ambientLight );
+  const instances = 50000;
 
-const light1 = new THREE.DirectionalLight( 0xffffff, 0.5 );
-light1.position.set( 1, 1, 1 );
-scene.add( light1 );
+	const positions = [];
+	const offsets = [];
+	const colors = [];
+	const orientationsStart = [];
+	const orientationsEnd = [];
+	
+  positions.push( 0.025, - 0.025, 0 );
+	positions.push( - 0.025, 0.025, 0 );
+	positions.push( 0, 0, 0.025 );
 
-const light2 = new THREE.DirectionalLight( 0xffffff, 1.5 );
-light2.position.set( 0, - 1, 0 );
-scene.add( light2 );
+	// instanced attributes
 
-const triangles = 500000;
-///
-const geometry = new THREE.BufferGeometry();
+	for ( let i = 0; i < instances; i ++ ) {
+			// offsets
+		offsets.push( Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5 );
+			// colors
+		colors.push( Math.random(), Math.random(), Math.random(), Math.random() );
+			// orientation start
+		vector.set( Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1 );
+		vector.normalize();
 
-const positions = [];
-const normals = [];
-const colors = [];
+		orientationsStart.push( vector.x, vector.y, vector.z, vector.w );
 
-const color = new THREE.Color();
+			// orientation end
+		vector.set( Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1 );
+		vector.normalize();
 
-const n = 800, n2 = n / 2;	// triangles spread in the cube
-const d = 12, d2 = d / 2;	// individual triangle size
+		orientationsEnd.push( vector.x, vector.y, vector.z, vector.w );
+  };
 
-const pA = new THREE.Vector3();
-const pB = new THREE.Vector3();
-const pC = new THREE.Vector3();
+	// geometry.instanceCount = instances; // set so its initalized for dat.GUI, will be set in first draw otherwise
+	geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( positions, 3 ) );
+	geometry.setAttribute( 'offset', new THREE.InstancedBufferAttribute( new Float32Array( offsets ), 3 ) );
+	geometry.setAttribute( 'color', new THREE.InstancedBufferAttribute( new Float32Array( colors ), 4 ) );
+	geometry.setAttribute( 'orientationStart', new THREE.InstancedBufferAttribute( new Float32Array( orientationsStart ), 4 ) );
+	geometry.setAttribute( 'orientationEnd', new THREE.InstancedBufferAttribute( new Float32Array( orientationsEnd ), 4 ) );
 
-const cb = new THREE.Vector3();
-const ab = new THREE.Vector3();
+	// material
+    
+	const material = new THREE.RawShaderMaterial( {
+    uniforms: {
+      'time': { value: 1.0 },
+			'sineTime': { value: 1.0 }
+    },
+		vertexShader: document.getElementById( 'vertexShader' ).textContent,
+		fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
+		side: THREE.DoubleSide,
+		forceSinglePass: true,
+		transparent: true
+  } );
 
-for ( let i = 0; i < triangles; i ++ ) {
+	const mesh = new THREE.Mesh( geometry, material );
+	scene.add( mesh );
 
-// positions
+  function animate() {
+		requestAnimationFrame( animate );
+		
+    render();
+	}
 
-const x = Math.random() * n - n2;
-const y = Math.random() * n - n2;
-const z = Math.random() * n - n2;
+	function render() {
 
-const ax = x + Math.random() * d - d2;
-const ay = y + Math.random() * d - d2;
-const az = z + Math.random() * d - d2;
+		const time = performance.now();
+		
+    const object = scene.children[ 0 ];
+		
+    object.rotation.y = time * 0.0005;
+		object.material.uniforms[ 'time' ].value = time * 0.005;
+		object.material.uniforms[ 'sineTime' ].value = Math.sin( object.material.uniforms[ 'time' ].value * 0.05 );
 
-const bx = x + Math.random() * d - d2;
-const by = y + Math.random() * d - d2;
-const bz = z + Math.random() * d - d2;
+    renderer.render( scene, camera );
+	}
 
-const cx = x + Math.random() * d - d2;
-const cy = y + Math.random() * d - d2;
-const cz = z + Math.random() * d - d2;
 
-positions.push( ax, ay, az );
-positions.push( bx, by, bz );
-positions.push( cx, cy, cz );
+	animate();
 
-// flat face normals
-
-pA.set( ax, ay, az );
-pB.set( bx, by, bz );
-pC.set( cx, cy, cz );
-
-cb.subVectors( pC, pB );
-ab.subVectors( pA, pB );
-cb.cross( ab );
-
-cb.normalize();
-
-const nx = cb.x;
-const ny = cb.y;
-const nz = cb.z;
-
-normals.push( nx * 32767, ny * 32767, nz * 32767 );
-normals.push( nx * 32767, ny * 32767, nz * 32767 );
-normals.push( nx * 32767, ny * 32767, nz * 32767 );
-
-// colors
-
-const vx = ( x / n ) + 0.5;
-const vy = ( y / n ) + 0.5;
-const vz = ( z / n ) + 0.5;
-
-color.setRGB( vx, vy, vz );
-
-colors.push( color.r * 255, color.g * 255, color.b * 255 );
-colors.push( color.r * 255, color.g * 255, color.b * 255 );
-colors.push( color.r * 255, color.g * 255, color.b * 255 );
-
-}
-
-const positionAttribute = new THREE.Float32BufferAttribute( positions, 3 );
-const normalAttribute = new THREE.Int16BufferAttribute( normals, 3 );
-const colorAttribute = new THREE.Uint8BufferAttribute( colors, 3 );
-
-normalAttribute.normalized = true; // this will map the buffer values to 0.0f - +1.0f in the shader
-colorAttribute.normalized = true;
-
-geometry.setAttribute( 'position', positionAttribute );
-geometry.setAttribute( 'normal', normalAttribute );
-geometry.setAttribute( 'color', colorAttribute );
-
-geometry.computeBoundingSphere();
-////////
-const material = new THREE.MeshPhongMaterial( {
-  color: 0xaaaaaa, 
-  specular: 0xffffff, 
-  shininess: 250,
-	side: THREE.DoubleSide, 
-  vertexColors: true,
-} );
-
-const mesh = new THREE.Mesh( geometry, material );
-scene.add( mesh );
-////////
-
-	function animate() {
-  requestAnimationFrame( animate );
-  
-  // function render() {
-  const time = Date.now() * 0.001;
-  
-  mesh.rotation.x = time * 0.025;
-	mesh.rotation.y = time * 0.005;
-
-	renderer.render( scene, camera );
-  }
-
-  animate();
-
-  function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-  }
+	function onWindowResize() {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize( window.innerWidth, window.innerHeight );
+	};
   window.addEventListener( 'resize', onWindowResize );
 
-  //orbitcontrols
-  const controls = new OrbitControls(camera, renderer.domElement);
-  controls.minDistance = 20;
-  controls.maxDistance = 100;
-  controls.minPolarAngle = 1;
-  controls.maxPolarAngle = Math.PI / 1.5;
-  controls.update();
+//   //orbitcontrols
+//   const controls = new OrbitControls(camera, renderer.domElement);
+//   controls.minDistance = 20;
+//   controls.maxDistance = 100;
+//   controls.minPolarAngle = 1;
+//   controls.maxPolarAngle = Math.PI / 1.5;
+//   controls.update();
 
 } else {
   var warning = WEBGL.getWebGLErrorMessage()
