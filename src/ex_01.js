c0f0d8c05f94bd2c70096a37403ed22c756a7dac
tiredgroove <tiredgroove@gmail.com>
2023-04-19 11:08:56 +0700

    111

--- src/ex_01.js
+++ src/ex_01.js
@@ -4,18 +4,18 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 
 if (WEBGL.isWebGLAvailable()) {
 //////
-
     ///scene
-  const scene = new THREE.Scene();
-	// scene.background = new THREE.Color( 0xffffff );
-  // scene.fog = new THREE.Fog( 0xffffff, 2000, 3500 );
+	const scene = new THREE.Scene();
+	scene.background = new THREE.Color( 0xffffff );
+	scene.fog = new THREE.FogExp2( 0xe3ff4b, 0.002 );
     //camera
-  const camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 10 );
-	camera.position.z = 2;
+	const camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 10 );
+	camera.position.z = 3;
     //renderer
 	const renderer = new THREE.WebGLRenderer();
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.outputEncoding = THREE.sRGBEncoding;
 	document.body.appendChild( renderer.domElement );
 
 		// geometry
@@ -24,14 +24,35 @@ if (WEBGL.isWebGLAvailable()) {
   const vector = new THREE.Vector4();
 
   const instances = 50000;
-
-	const positions = [];
+//rectanglar
+	const positions = [
+	-0.015, -0.015, 0.0,
+   0.015, -0.015, 0.0,
+   0.015,  0.015, 0.0,
+  -0.015,  0.015, 0.0		
+	];
 	const offsets = [];
 	const colors = [];
+	const colorsArray = [
+		new THREE.Color(0xff146b), //rose
+		new THREE.Color(0xffe010), //yellow
+		new THREE.Color(0x65e9d3), //warm cyan
+		new THREE.Color(0x2f56c0), //warm navy
+		new THREE.Color(0xc9bfa2), //beige
+		new THREE.Color(0x4d9659), //green
+		new THREE.Color(0x9fe5ff), //sky bleu
+		new THREE.Color(0xfcfacf), //cream
+		new THREE.Color(0xffc2e2), //light rose
+		new THREE.Color(0xbdbdbd), //light gray
+		new THREE.Color(0xf266ab), //medium rose
+		new THREE.Color(0x503919), //brown
+	  ];
+	const getRandomColor = () => colorsArray[Math.floor(Math.random() * colorsArray.length)];
+	//
 	const orientationsStart = [];
 	const orientationsEnd = [];
 	
-  positions.push( 0.025, - 0.025, 0 );
+ 	positions.push( 0.025, - 0.025, 0 );
 	positions.push( - 0.025, 0.025, 0 );
 	positions.push( 0, 0, 0.025 );
 
@@ -41,7 +62,8 @@ if (WEBGL.isWebGLAvailable()) {
 			// offsets
 		offsets.push( Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5 );
 			// colors
-		colors.push( Math.random(), Math.random(), Math.random(), Math.random() );
+		// colors.push( Math.random(), Math.random(), Math.random(), Math.random() );
+		colors.push(getRandomColor().r, getRandomColor().g, getRandomColor().b, 1);
 			// orientation start
 		vector.set( Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1 );
 		vector.normalize();
@@ -61,7 +83,7 @@ if (WEBGL.isWebGLAvailable()) {
 	geometry.setAttribute( 'color', new THREE.InstancedBufferAttribute( new Float32Array( colors ), 4 ) );
 	geometry.setAttribute( 'orientationStart', new THREE.InstancedBufferAttribute( new Float32Array( orientationsStart ), 4 ) );
 	geometry.setAttribute( 'orientationEnd', new THREE.InstancedBufferAttribute( new Float32Array( orientationsEnd ), 4 ) );
-
+	geometry.setIndex([0, 1, 2, 0, 2, 3]); //rectanglar
 	// material
     
 	const material = new THREE.RawShaderMaterial( {
@@ -79,7 +101,7 @@ if (WEBGL.isWebGLAvailable()) {
 	const mesh = new THREE.Mesh( geometry, material );
 	scene.add( mesh );
 
-  function animate() {
+  	function animate() {
 		requestAnimationFrame( animate );
 		
     render();
@@ -87,13 +109,15 @@ if (WEBGL.isWebGLAvailable()) {
 
 	function render() {
 
-		const time = performance.now();
-		
+	const time = performance.now();
     const object = scene.children[ 0 ];
 		
-    object.rotation.y = time * 0.0005;
-		object.material.uniforms[ 'time' ].value = time * 0.005;
-		object.material.uniforms[ 'sineTime' ].value = Math.sin( object.material.uniforms[ 'time' ].value * 0.05 );
+    object.rotation.y = time * 0.00005;
+	object.rotation.x = time * 0.00005;
+	object.rotation.z = time * 0.00005;
+	
+	object.material.uniforms[ 'time' ].value = time * 0.0025;
+	object.material.uniforms[ 'sineTime' ].value = Math.sin( object.material.uniforms[ 'time' ].value * 0.05 );
 
     renderer.render( scene, camera );
 	}
@@ -101,20 +125,20 @@ if (WEBGL.isWebGLAvailable()) {
 
 	animate();
 
+	//orbitcontrols
+	const controls = new OrbitControls(camera, renderer.domElement);
+	controls.minDistance = 1;
+	controls.maxDistance = 5;
+	controls.minPolarAngle = 1;
+	controls.maxPolarAngle = Math.PI / 1.5;
+	controls.update();
+
 	function onWindowResize() {
 		camera.aspect = window.innerWidth / window.innerHeight;
 		camera.updateProjectionMatrix();
 		renderer.setSize( window.innerWidth, window.innerHeight );
 	};
-  window.addEventListener( 'resize', onWindowResize );
-
-//   //orbitcontrols
-//   const controls = new OrbitControls(camera, renderer.domElement);
-//   controls.minDistance = 20;
-//   controls.maxDistance = 100;
-//   controls.minPolarAngle = 1;
-//   controls.maxPolarAngle = Math.PI / 1.5;
-//   controls.update();
+	window.addEventListener( 'resize', onWindowResize );
 
 } else {
   var warning = WEBGL.getWebGLErrorMessage()
